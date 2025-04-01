cb75a6b02eb316c2abfb8961e7c44cae224b23dc
SrakhiuMeow <srakhiumeow@gmail.com>
2025-04-01 22:07:17 +0800

    feat: 增加显示文章功能

--- scripts/lofter-history.js
+++ scripts/lofter-history.js
@@ -13,6 +13,78 @@
 (function () {
     'use strict';
 
+    function buildArticleElement(blogUrl, avatarUrl, publisher, imageUrl, digest, tags, postUrl, title) {
+        const avatar = document.createElement('div');
+        avatar.className = 'mlistimg';
+        avatar.innerHTML = `
+            <div class="w-img" style="z-index:1;"> 
+                <a href="${blogUrl}" target="_blank">
+                    <img src="${avatarUrl}?imageView&amp;thumbnail=64x64&amp;type=jpg">
+                </a>
+            </div>
+            <div class="w-img" style="height:0px; padding:0; z-index:10;"></div>
+        `;
+
+        const notDisplayImage = typeof imageUrl == "undefined" || imageUrl == null || imageUrl == "";
+        const content = document.createElement('div');
+        content.className = 'mlistcnt';
+        content.innerHTML = `
+            <div class="isay">
+                <div class="isayt"> <a class="isayc" href="${postUrl}" title="查看全文" target="_blank">打开新页</a></div>
+                <div class="isaym">
+                    <div class="w-who"><a href="${blogUrl}" class="publishernick"
+                            target="_blank">${publisher}</a> 
+                    </div>
+                    <div class="js">
+                        <div class="m-icnt">
+                            <h2 class="tit"> ${title}</h2>
+                            <div class="cnt">
+                                <div class="img" style="width: 164px; height: auto; display: ${notDisplayImage ? "none" : "block"}">
+                                    <div class="imgc"> <a href="#" hidefocus="true"><img 
+                                                style="width:164px;"
+                                                src="${imageUrl}?imageView&amp;thumbnail=1000x0&amp;type=jpg"></a>
+                                        <div class="sphotolabels" style="display:none"></div>
+                                    </div>
+                                    <a class="w-zoom">查看大图</a>
+                                </div>
+                            </div>
+                            <div class="txt" style="display: block;">
+                                ${digest}
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="w-opt">
+                        <div class="opta" style="width: 132px;">
+                            ${tags}
+                        </div>
+                        <div class="optb"> <span class="opti" style="display: none;">
+                            <span class="opti">
+                                <a href="${postUrl}" target="_blank" hidefocus="true">查看全文</a>
+                                <span class="opticrt"></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="isayb"></div>
+            </div>
+        `;
+
+
+        const article = document.createElement('div');
+        article.className = 'm-mlist';
+        article.appendChild(avatar);
+        article.appendChild(content);
+        
+        return article;        
+    }
+
+    function insertArticle(article) {
+        const main = document.getElementById('main');
+        main.insertBefore(article, main.children[7]);
+    }
+
+
     function getHistory() {
         console.log('hi');
     }
