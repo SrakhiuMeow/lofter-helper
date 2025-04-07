9942d3ec1cd9409ae77210d62a157ecfb2353f65
SrakhiuMeow <srakhiumeow@gmail.com>
2025-04-07 13:45:17 +0800

    feat: 实现查看合集内容，但是仅支持原主题

--- scripts/lofter-collection.js
+++ scripts/lofter-collection.js
@@ -12,10 +12,10 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    function subscribe(authkey, collectionId, mode=true) {
+    function subscribe(authkey, collectionId, mode = true) {
         // 订阅合集
         // mode = true 订阅， mode = false 取消订阅
         const url = new URL("https://api.lofter.com/v2.0/subscribeCollection.api");
@@ -42,16 +42,16 @@
                     'content-type': "application/x-www-form-urlencoded; charset=utf-8",
                     'lofter-phone-login-auth': authkey,
                 },
-                onload: function(response) {
+                onload: function (response) {
                     try {
-                        console.log(response);
+                        // console.log(response);
                         const data = JSON.parse(response.responseText);
                         resolve(data.response);
                     } catch (e) {
                         reject(e);
                     }
                 },
-                onerror: function(error) {
+                onerror: function (error) {
                     reject(error);
                 }
             });
@@ -59,20 +59,22 @@
     }
 
     function getCollection(authkey, blogid, blogdomain, offset = 0, limit = 20) {
+        // 获取用户合集列表
+
         const url = new URL("https://api.lofter.com/v1.1/postCollection.api");
         const params = {
             'method': 'getCollectionList',
             'needViewCount': 1,
             // 'blogid': blogid,
             'blogdomain': blogdomain,
-            'product': 'lofter-android-8.1.25'
+            'product': 'lofter-android-7.6.12'
         };
 
         Object.keys(params).forEach(key =>
             url.searchParams.append(key, params[key])
         );
 
-        console.log(authkey);
+        // console.log(authkey);
         return new Promise((resolve, reject) => {
             GM.xmlHttpRequest({
                 method: "GET",
@@ -82,21 +84,65 @@
                     'content-type': "application/x-www-form-urlencoded; charset=utf-8",
                     'lofter-phone-login-auth': authkey,
                 },
-                onload: function(response) {
+                onload: function (response) {
                     try {
-                        console.log(response);
+                        // console.log(response);
                         const data = JSON.parse(response.responseText);
                         resolve(data.response);
                     } catch (e) {
                         reject(e);
                     }
                 },
-                onerror: function(error) {
+                onerror: function (error) {
                     reject(error);
                 }
             });
         });
     }
+
+    function getCollectionDetail(authkey, collectionId, offset, limit = 15, order = 1) {
+        // 获取某一合集详情
+
+        const url = new URL("https://api.lofter.com/v1.1/postCollection.api");
+        const params = {
+            'method': 'getCollectionDetail',
+            'product': 'lofter-android-7.6.12',
+            'offset': offset,
+            'limit': limit,
+            'collectionid': collectionId,
+            'order': order,
+        };
+
+        Object.keys(params).forEach(key =>
+            url.searchParams.append(key, params[key])
+        );
+
+        // console.log(authkey);
+        return new Promise((resolve, reject) => {
+            GM.xmlHttpRequest({
+                method: "GET",
+                url: url.toString(),
+                headers: {
+                    'Accept-Encoding': "br,gzip",
+                    'content-type': "application/x-www-form-urlencoded; charset=utf-8",
+                    'lofter-phone-login-auth': authkey,
+                },
+                onload: function (response) {
+                    try {
+                        // console.log(response);
+                        const data = JSON.parse(response.responseText);
+                        resolve(data.response);
+                    } catch (e) {
+                        reject(e);
+                    }
+                },
+                onerror: function (error) {
+                    reject(error);
+                }
+            });
+        });
+    }
+
 
     function getCookie(name) {
         const cookies = document.cookie.split('; ');
@@ -111,19 +157,19 @@
 
     function formatTimestamp(timestamp) {
         const date = new Date(timestamp);
-        
+
         const pad = num => num.toString().padStart(2, '0');
-        
+
         // return `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDate())} ` +
         //        `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
-        return `${pad(date.getMonth()+1)}-${pad(date.getDate())}`;
+        return `${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
     }
 
     const authkey = getCookie("LOFTER-PHONE-LOGIN-AUTH");
     const blogdomain = window.location.hostname;
     const publisher = blogdomain.split('.')[0];
-    const offset = 0;
-    const controlFrame = document.getElementById('control_frame');
+
+    // const controlFrame = document.getElementById('control_frame');
 
     // subscribe(authkey, '')
     //     .then(response => {
@@ -134,13 +180,15 @@
     //     });
 
     function displayCollection(collections) {
+        // 显示合集列表
+
         const postwrapper = document.querySelector('div.postwrapper');
         // const page = document.querySelector('div.page');
 
         collections.forEach(collection => {
             const block = document.createElement('div');
             block.className = 'block article';
-            
+
             const side = document.createElement('div');
             side.className = 'side';
             const main = document.createElement('div');
@@ -166,7 +214,7 @@
                 tagElement.target = '_blank';
                 tag.appendChild(tagElement);
             });
-            
+
 
             const text = document.createElement('div');
             text.className = 'text';
@@ -177,7 +225,7 @@
 
             const collectionDetail = `https://www.lofter.com/collection/${publisher}/?op=collectionDetail&collectionId=${collection.id}&sort=0`;
             const collectionUrl = `https://www.lofter.com/front/blog/collection/share?collectionId=${collection.id}`;
-            
+
             img.innerHTML = `<img src="${collection.coverUrl}?imageView&thumbnail=70x70&quality=90&type=jpg">`;
 
             text.innerHTML = `
@@ -185,14 +233,14 @@
                 <p>${collection.description}</p>
             `;
 
-            
+
             link.innerHTML = `
-                <a>ID:${collection.id}</a>
-                <a>${collection.postCount}篇)/a>
+                <a>复制ID</a>
+                <a>${collection.postCount}篇</a>
                 <a>${collection.viewCount}浏览</a>
                 <a>${collection.postCollectionHot}热度</a>
                 <a>${formatTimestamp(collection.updateTime)}更新</a>
-                <a href="${collectionDetail}" target="_blank">查看详情</a>
+                <a>查看详情</a>
             `;
 
             // const subscribeButton = link.querySelector('a:last-child');
@@ -223,33 +271,108 @@
             //     }
             // });
 
+            const copyButton = link.querySelector('a:first-child');
+            copyButton.addEventListener('click', () => {
+                navigator.clipboard.writeText(collection.id).then(() => {
+                    // console.log('已将合集ID复制到剪切板:', collection.id);
+                    alert('已将合集ID复制到剪切板:' + collection.id);
+                }).catch(err => {
+                    console.error('复制失败:', err);
+                });
+            });
+            copyButton.style.cursor = 'pointer';
+
+            const detailButton = link.querySelector('a:last-child');
+
+            const list = document.createElement('div');
+            main.appendChild(list);
+            if (collection.postCount === 0) {
+                detailButton.innerHTML = '没有内容了';
+                detailButton.style.cursor = 'not-allowed';
+                detailButton.removeEventListener('click', this);
+                detailButton.remove();
+                return;
+            }
+            const br = document.createElement('br');
+            list.appendChild(br);
+
+            detailOffsets[collection.id] = 0;
+            detailButton.addEventListener('click', () => {
+                getCollectionDetail(authkey, collection.id, detailOffsets[collection.id])
+                    .then(response => {
+                        // console.log("collection detail response:", response);
+
+                        // 处理合集详情
+                        displayCollectionDetail(response.items, list);
+
+                        if (detailOffsets[collection.id] >= collection.postCount) {
+                            detailButton.innerHTML = '没有更多内容了';
+                            detailButton.style.cursor = 'not-allowed';
+                            const clone = detailButton.cloneNode(true);
+                            detailButton.parentNode.replaceChild(clone, detailButton);
+                            return;
+                        } else {
+                            detailButton.innerHTML = '加载更多';
+                            detailButton.style.cursor = 'pointer';
+                        }
+
+                    })
+                    .catch(error => console.error(error));
+                detailOffsets[collection.id] += 15;
+
+            });
+            detailButton.style.cursor = 'pointer';
+
             // postwrapper.insertBefore(block, page);
             postwrapper.appendChild(block);
         });
     }
 
+    const detailOffsets = {};
+
+    function displayCollectionDetail(items, list) {
+        // 显示合集详情
+
+
+        items.forEach(item => {
+            const link = document.createElement('a');
+            link.href = item.post.blogPageUrl;
+            link.target = '_blank';
+            link.innerHTML = item.post.title;
+
+            list.appendChild(link);
+            list.innerHTML += '<br>';
+
+        });
+
+    }
+
 
     function change2collection() {
+        // 将页面修改为显示合集列表模式
+
         this.innerHTML = '<a>返回</a>';
         this.addEventListener('click', () => {
             window.location.reload();
         });
-        
-        
+
+
         // 清除原有内容
         const postwrapper = document.querySelector('div.postwrapper');
         const postElements = postwrapper.querySelectorAll('div.block');
         postElements.forEach(element => element.remove());
         const page = document.querySelector('div.page');
         postwrapper.removeChild(page);
-        
 
-        getCollection(authkey, '', blogdomain, offset, 20)
+        // 获取合集列表
+        getCollection(authkey, '', blogdomain)
             .then(response => displayCollection(response.collections))
             .catch(error => console.error(error));
     }
 
     function initialize() {
+        // 初始化
+
         // 添加合集按钮
         const sidelist = document.querySelector('ul.sidelist');
         const collectionButton = document.createElement('li');
@@ -257,33 +380,33 @@
         collectionButton.addEventListener('click', change2collection);
         collectionButton.style.cursor = 'pointer';
         sidelist.appendChild(collectionButton);
-        
-        
+
+
     }
 
-    
+
 
     // 监听 DOM 变化，等待 ul.sidelist 加载完成
     function waitForElement(selector, callback) {
         const observer = new MutationObserver((mutations, obs) => {
-          const element = document.querySelector(selector);
-          if (element) {
-            obs.disconnect(); // 停止观察
-            callback(element);
-          }
+            const element = document.querySelector(selector);
+            if (element) {
+                obs.disconnect(); // 停止观察
+                callback(element);
+            }
         });
-      
+
         // 开始观察整个文档的变化
         observer.observe(document, {
-          childList: true,
-          subtree: true
+            childList: true,
+            subtree: true
         });
-      }
-      
-    
-      waitForElement('ul.sidelist', (element) => {
-        console.log('组件已加载:', element);
+    }
+
+    // 避免脚本过早执行
+    waitForElement('ul.sidelist', (element) => {
+        // console.log('组件已加载:', element);
         initialize();
-      });
+    });
 
 })();
